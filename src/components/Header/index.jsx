3a2f74640deafc12b378bea131eaf74c766d727d
almapowell <49799618+almapowell@users.noreply.github.com>
2020-05-16 14:47:58 -0600

    initail header has been set up

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {HeaderWrapper, NavLinks, HomeDiv,HomeLink,NavElement} from './styles';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <HeaderWrapper>
+            <HomeDiv>
+                <HomeLink exact to="/" activeClassName="main-nav-active">Home</HomeLink> 
+            </HomeDiv>
+            <NavLinks>
+                <NavElement to="/add-project" activeClassName="main-nav-active">Add Project</NavElement> 
+                <NavElement to="/search-listings" activeClassName="main-nav-active">Search Listings</NavElement> 
+                <NavElement to="/login" activeClassName="main-nav-active">Login</NavElement> 
+            </NavLinks>
+        </HeaderWrapper>
+
     )
 }
 export default Header;
